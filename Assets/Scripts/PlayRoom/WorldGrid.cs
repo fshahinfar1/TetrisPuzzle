ee4c35fa851c91ed0ee55852cd3d20376fe9c5d3
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-26 15:26:00 +0430

    Update WorldGrid script to instantiatee tiles needed for the game.

--- Assets/Scripts/PlayRoom/WorldGrid.cs
+++ Assets/Scripts/PlayRoom/WorldGrid.cs
@@ -9,13 +9,37 @@ namespace PlayRoom
         [SerializeField] int row;
         [SerializeField] int column;
 
+        [SerializeField] Transform contents;
+
+
         WorldMatrice matrice;
 
         void Awake()
         {
             matrice = new WorldMatrice(row, column);
+            InstantiateTiles();
         }
 
+        void InstantiateTiles()
+        {
+            var tile = Resources.Load<GameObject>("PlayRoom/Tile");
+            int count = row * column;
+            for (int i = 0; i < count; i++) {
+                var newObj = Instantiate(tile);
+                var trans = newObj.transform;
+                trans.SetParent(contents);
+                //trans.SetAsLastSibling();
+            }
 
+            // add grid view
+            var contentsObj = contents.gameObject;
+            var grid = contentsObj.AddComponent<GridView>();
+            grid.contents = contents;
+            Debug.Log("Set contents");
+            grid.rowNum = row;
+            grid.rowHeight = 0.8f;
+            grid.colWidth = 0.8f;
+            grid.SetupGrid(true);
+        }
     }
 }
