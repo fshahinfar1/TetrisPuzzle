54404b63de86e481072742f6d481a7e9a4de7b26
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-27 13:10:58 +0430

    Add a manager for the game

---
+++ Assets/Scripts/PlayRoom/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayRoom.Blocks;
+
+namespace PlayRoom
+{
+    public class Manager : MonoBehaviour
+    {
+        BlockGenerator blockGen;
+        void Start()
+        {
+            blockGen = (BlockGenerator)General.RefBook.Summon("BlockGenerator");
+            blockGen.Generate();
+        }
+    }
+}
