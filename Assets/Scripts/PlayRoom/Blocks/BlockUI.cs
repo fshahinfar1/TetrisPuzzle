943af77c8544819d79c8de3369526cd4c4ec2c36
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-27 13:10:11 +0430

    Add a class to handle a block ui including drag and drop actions

---
+++ Assets/Scripts/PlayRoom/Blocks/BlockUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PlayRoom.Blocks
+{
+    public class BlockUI : MonoBehaviour, 
+        IDragHandler, IBeginDragHandler, IEndDragHandler
+    {
+        BaseBlock block;
+        Vector3 basePosition;
+
+        public void SetBlock(BaseBlock b)
+        {
+            block = b;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            basePosition = transform.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            var pos = Input.mousePosition;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos.z = 0;
+            transform.position = pos;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            transform.position = basePosition;
+        }
+    }
+}
