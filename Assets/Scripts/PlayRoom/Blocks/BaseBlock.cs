634ccde9ae2deac4c142cb07730ae69dfae8cd14
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-27 12:14:11 +0430

    Write a simple class to handle base block behavioure

---
+++ Assets/Scripts/PlayRoom/Blocks/BaseBlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayRoom.Blocks
+{
+    public class BaseBlock
+    {
+        static WorldGrid worldGrid;
+        int size;
+        Coordinate[] relativePos;
+        Sprite sprite;
+        int insertIndex;
+
+        public BaseBlock(int size)
+        {
+            insertIndex = 0;
+            this.size = size;
+            relativePos = new Coordinate[size];
+        }
+
+        public void SetSprite(Sprite s)
+        {
+            sprite = s;
+        }
+
+        public void AddRelativePos(Coordinate c)
+        {
+            relativePos[insertIndex++] = c;
+        }
+
+        public void SetGrid(Coordinate coordinate)
+        {
+            if (worldGrid == null)
+            {
+                worldGrid = (WorldGrid)General.RefBook.Summon("WorldGrid");
+            }
+            foreach (Coordinate delta in relativePos) {
+                int row = coordinate.row + delta.row;
+                int col = coordinate.column + delta.column;
+                worldGrid.SetTiles(row, col, sprite);
+            }
+        }
+    }
+}
