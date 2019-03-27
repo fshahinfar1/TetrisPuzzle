cfd7c7c5a66e0be06dd2b6193d421207a9e5e8ab
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-27 13:09:37 +0430

    Implement a factory class for building base block objects

---
+++ Assets/Scripts/PlayRoom/Blocks/BlockFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayRoom.Blocks
+{
+    public class BlockFactory
+    {
+        public static BaseBlock Build(string name, Sprite s)
+        {
+            if (name == "1-Block")
+            {
+                var block = new BaseBlock(1);
+                block.AddRelativePos(new Coordinate(0, 0));
+                block.SetSprite(s);
+                return block;
+            }
+            else if (name == "3-Block")
+            {
+                var block = new BaseBlock(3);
+                block.AddRelativePos(new Coordinate(0, 0));
+                block.AddRelativePos(new Coordinate(1, 0));
+                block.AddRelativePos(new Coordinate(2, 0));
+                block.SetSprite(s);
+                return block;
+            }
+            else if (name == "4-Block")
+            {
+                var block = new BaseBlock(4);
+                block.AddRelativePos(new Coordinate(0, 0));
+                block.AddRelativePos(new Coordinate(0, 1));
+                block.AddRelativePos(new Coordinate(1, 0));
+                block.AddRelativePos(new Coordinate(1, 1));
+                block.SetSprite(s);
+                return block;
+            }
+            return null;
+        }
+    }
+}
