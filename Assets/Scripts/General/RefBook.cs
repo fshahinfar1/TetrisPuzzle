da9cd7798d16091b7e4c110d4fd0790a6455d251
fshahinfar1 <fsh1.dev@gmail.com>
2019-03-27 11:35:04 +0430

    Add a class for book keeping refrence of objects during the game.

---
+++ Assets/Scripts/General/RefBook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace General
+{
+    public class RefBook
+    {
+        static Dictionary<string, object> masterBook;
+
+        public static void Register(string key, object refrence)
+        {
+            masterBook.Add(key, refrence);
+        }
+
+        public static object Summon(string key)
+        {
+            return masterBook[key];
+        }
+    }
+}
